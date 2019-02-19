e978a112038f528dbdbf46d356aaa4ec13c710bb
Karen Shea <shea.karene@gmail.com>
2019-02-18 20:59:35 -0500

    Update 2/18

--- routes/index.js
+++ routes/index.js
@@ -10,20 +10,28 @@ router.get('/', (req, res) => {
     res.render('index')
   });
 
-//-An "about" route (/about) to render the "About" page
-router.get('/about', (req, res) => {
-    res.render('about');
-  });
-
-  //-An "index" route (/) to render the "Home" page
+// Render the Index Page
   router.get('/index', (req, res) => {
     res.render('index');
     res.app.locals = data.projects;
 });
 
+// Render the About Page
+router.get('/about', (req, res) => {
+  res.render('about');
+  res.app.locals = data.projects;
+});
+
+// Dynamic "project" routes (/project or /projects) based on the id of the project that render a customized
+  // version of the Pug project template to show off each project. 
+  // Which means adding data, or "locals", as an object that contains data to be passed to the Pug template.
+
+ // Unsure how to do this...
+
+router.get('', (req, res) => {
 
 
-
+});
 
 
 
